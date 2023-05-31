0898570b6f6fdc9bc18aa4800542641bec5317bd
palejrey@gmail.com <palejrey@gmail.com>
2023-05-31 15:06:39 -0500

    Added all necessary files for the routes folder

---
+++ routes/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const friendRoutes = require('./friendRoutes.js');
+const userRoutes = require('./userRoutes');
+const reactionRoutes = require('./reactionRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/friends', friendRoutes);
+router.use('/users', userRoutes);
+router.use('/reactions', reactionRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
