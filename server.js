1b7b29cf532804c57a91001f18a541e261bd3048
palejrey@gmail.com <palejrey@gmail.com>
2023-05-31 14:41:22 -0500

    Setup server.js and added script in package.json for starting server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const db = require('./config/connection');
+
+const { Reaction, Thought, User } = require('./models');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
